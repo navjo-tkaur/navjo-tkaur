7f1ce273b4c4d95f5b7977473dce4e34668bdc2a
navjo-tkaur <N.kaur3654@student.leedsbeckett.ac.uk>
2025-05-05 07:54:02 +0100

    Add files via upload

---
+++ temperature_converter.py
@@ -0,0 +1,12 @@
+# temperature_converter.py
+# This program converts a temperature from Celsius to Fahrenheit
+
+# Temperature in Celsius
+celsius = 38.4
+
+# Convert Celsius to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Display both temperatures
+print(f"Temperature in Celsius: {celsius}°C")
+print(f"Equivalent in Fahrenheit: {fahrenheit:.2f}°F")
